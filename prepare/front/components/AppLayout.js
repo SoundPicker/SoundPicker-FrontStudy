acd23dfe024fa54954453e2b0909403dc835699b
ksmself <53217988+ksmself@users.noreply.github.com>
2021-01-31 20:31:06 +0900

    섹션 1. _app.js와 Head

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,21 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
+import { Menu } from "antd";
 
+// 특정 컴포넌트끼리 공통인 애는 레이아웃으로 만들어
+// 개별 컴포넌트를 감싸면 됨
 const AppLayout = ({ children }) => {
   return (
     <div>
-      <div>
-        <Link href="/">
-          <a>노드버드</a>
-        </Link>
-        <Link href="/profile">
-          <a>프로필</a>
-        </Link>
-        <Link href="/signup">
-          <a>회원가입</a>
-        </Link>
-      </div>
+      <Menu mode="horizontal">
+        <Menu.Item>
+          <Link href="/">
+            <a>노드버드</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/profile">
+            <a>프로필</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/signup">
+            <a>회원가입</a>
+          </Link>
+        </Menu.Item>
+      </Menu>
       {children}
     </div>
   );
---
+++ prepare/front/pages/_app.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import "antd/dist/antd.css";
+
+// Component에 index.js, profile.js ... 등이 들어감
+// 쟤들의 부모인 셈. 모든 페이지에서 공통으로 적용하고 싶은 게 있다면 이런 식으로
+const NodeBird = ({ Component }) => {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>NodeBird</title>
+      </Head>
+      <Component />
+    </>
+  );
+};
+
+// props 있으면 점검을 위해
+NodeBird.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+};
+
+export default NodeBird;
