9b27c354205dcf87cbf81194cc07c0726f130775
ahyeonjoung <55370149+ahyeonjoung@users.noreply.github.com>
2021-02-03 14:36:53 +0900

    sextion 2. redux 설치

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -2,6 +2,7 @@ import 'antd/dist/antd.css'
 import PropTypes from 'prop-types';
 import Head from 'next/head'; //next에서 제공하는 헤드 수정 컴포넌트 
 import 'antd/dist/antd.css';
+import wrapper from '../store/configureStore'
 
 const App =({Component}) =>{ //pages들의 공통부분 
     return (
@@ -19,4 +20,4 @@ const App =({Component}) =>{ //pages들의 공통부분
 App.propTypes ={
     Component: PropTypes.elementType.isRequired,
 }
-export default App;
+export default wrapper.withRedux(App);
