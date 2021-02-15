36c644379ba6d8d688fbea370675834aff39c4a0
ksmself <53217988+ksmself@users.noreply.github.com>
2021-02-15 23:40:19 +0900

    섹션 2. 리덕스 설치와 필요성 소개

--- prepare/front/pages/_app.js
+++ prepare/front/pages/_app.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import Head from "next/head";
 import "antd/dist/antd.css";
 
+import wrapper from "../store/configureStore";
+
 // Component에 index.js, profile.js ... 등이 들어감
 // 쟤들의 부모인 셈. 모든 페이지에서 공통으로 적용하고 싶은 게 있다면 이런 식으로
 const NodeBird = ({ Component }) => {
@@ -22,4 +24,4 @@ NodeBird.propTypes = {
   Component: PropTypes.elementType.isRequired,
 };
 
-export default NodeBird;
+export default wrapper.withRedux(NodeBird);
