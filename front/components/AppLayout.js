4060fa860304f7ad0b7cb7242b1c2996e5f60462
NaLeum <wognsdl@likelion.org>
2021-03-21 23:47:32 +0900

    반응형그리드 사용하기.

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
+import {Menu, Input, Row, Col} from 'antd';
+
 
 const AppLayout = ({children}) => {
     return (
         <div>
-            <div>
-                <Link href='/'><a>노드버드</a></Link>
-                <Link href='/profile'><a>프로필</a></Link>
-                <Link href='/signup'><a>회원가입</a></Link>
-            </div>
-            {children}
+            <Menu mode='horizontal'>
+                <Menu.Item>
+                    <Link href='/'><a>노드버드</a></Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <Link href='/profile'><a>프로필</a></Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <Input.Search style={{verticalAlign:'middle'}} enterButton />
+                </Menu.Item>
+                <Menu.Item>
+                    <Link href='/signup'><a>회원가입</a></Link>
+                </Menu.Item>
+            </Menu>
+            <Row gutter={8}>
+                <Col xs={24} md={6}>
+                    왼쪽 메뉴
+                </Col>
+                <Col xs={24} md={12}>
+                    {children}
+                </Col>
+                <Col xs={24} md={6}>
+                    <a href="https://github.com/NaLeum" target="_blank" rel="noreferrer noopener">Go To NaLeum</a>
+                </Col>
+            </Row>
         </div>
     )
 };
--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,11 +1,18 @@
+import Head from 'next/head';
 import React from 'react';
 import AppLayout from '../components/AppLayout';
 
 const Signup = () => {
     return (
+        <>
+        <Head>
+            <meta charSet = "utf-8" />
+            <title>회원가입 | NodeBird</title>
+        </Head> 
         <AppLayout>
             <div>회원가입 페이지</div>
         </AppLayout>
+        </>
     );
 }
 
